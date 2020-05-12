4db3d1e9a1754c9334354134869d21f1722ff741
unknown <rahulk@selectiva.com>
2020-05-12 18:03:03 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
+  const [counter, setCounter] = React.useState(0);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Counter App</h1>
+      <div id="counter-value">{counter}</div>
+      <button id="increment-btn" onClick={() => setCounter(counter + 1)}>
+        Increment
+      </button>
+      <button
+        id="decrement-btn"
+        onClick={() => setCounter(counter > 0 ? counter - 1 : 0)}
+      >
+        Decrement
+      </button>
     </div>
   );
 }
--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,45 @@
-import React from 'react';
-import { render } from '@testing-library/react';
-import App from './App';
+import React from "react";
+import { render } from "@testing-library/react";
+import App from "./App";
+import { configure, shallow } from "enzyme";
+import Adapter from "enzyme-adapter-react-16";
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
+configure({ adapter: new Adapter() });
+let wrapper;
+beforeEach(() => {
+  wrapper = shallow(<App />);
+});
+test("renders learn react counter app", () => {
+  expect(wrapper.find("h1").text()).toContain("Counter App");
+
+  /* const { getByText } = render(<App />);
   const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  expect(linkElement).toBeInTheDocument(); */
+});
+
+test("render `Increment` counter button", () => {
+  expect(wrapper.find("#increment-btn").text()).toBe("Increment");
+});
+
+test("render `Decrement` counter button", () => {
+  expect(wrapper.find("#decrement-btn").text()).toBe("Decrement");
+});
+
+test("render initial `counter` value", () => {
+  expect(wrapper.find("#counter-value").text()).toBe("0");
+});
+
+test("render incremented `counter` value", () => {
+  wrapper.find("#increment-btn").simulate("click");
+  expect(wrapper.find("#counter-value").text()).toBe("1");
+});
+
+test("render decremented `counter` value", () => {
+  wrapper.find("#increment-btn").simulate("click");
+  wrapper.find("#decrement-btn").simulate("click");
+  expect(wrapper.find("#counter-value").text()).toBe("0");
+});
+test("render  `counter` value should be greter than `0`", () => {
+  wrapper.find("#decrement-btn").simulate("click");
+  expect(wrapper.find("#counter-value").text()).toBe("0");
 });
